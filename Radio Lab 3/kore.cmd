59c641529fa81e10e9fdd8851bc52326ce5a1210
Riya Kore <rykore@wisc.edu>
2024-11-29 14:00:00 +0530

    added data files for radio lab 3

---
+++ Radio Lab 3/kore.cmd
@@ -0,0 +1,40 @@
+: record lab3kore.rad
+: freq 1420.3 4
+: galactic 20 0
+: offset 0 30
+: noisecal
+: offset 0. 0.
+:300
+: galactic 24 0
+:300
+: galactic 26 0
+:300
+: galactic 28 0
+:300
+: galactic 32 0
+:300
+: galactic 36 0
+:300
+: galactic 40 0
+:300
+: galactic 44 0
+:300
+: galactic 48 0
+:300
+: galactic 52 0
+:300
+: galactic 56 0
+:300
+: galactic 60 0
+:300
+: galactic 64 0
+:300
+: galactic 68 0
+:300
+: galactic 72 0
+:300
+: galactic 76 0
+:300
+: roff
+
+
